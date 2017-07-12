dd8f490fa2c417c7c3b9c4eca986a8f6d3923cc6
Amir Zaidi <azaidi@live.nl>
2017-07-12 17:15:45 +0200

    Rewrite adb handling checks and add a spam prevent timeout

--- AudioStreamer/Program.cs
+++ AudioStreamer/Program.cs
@@ -2,7 +2,6 @@
 using CSCore.Streams;
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -15,36 +14,62 @@ namespace AudioStreamer
         static SoundInSource Source;
         static NetworkStream Stream;
         static byte[] Bytes = new byte[1024 * 1024];
-        static IPEndPoint End;
         static TaskCompletionSource<bool> DisconnectWaiter;
+        const int ServerPort = 1420;
 
         static void Main(string[] args)
         {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+
+            bool UseAdb = false;
+
+            try
+            {
+                var AdbDevices = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "adb",
+                    Arguments = "devices",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                });
+
+                AdbDevices.StandardOutput.ReadLine();
+                UseAdb = AdbDevices.StandardOutput.ReadLine().Trim() != string.Empty;
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+            }
+
+            IPAddress IPAddr;
+            if (UseAdb)
+                IPAddr = IPAddress.Loopback;
+            else
+            {
+                Console.Write("IP: ");
+                IPAddr = IPAddress.Parse(Console.ReadLine());
+            }
+
+            var Remote = new IPEndPoint(IPAddr, ServerPort);
             using (Capture = new WasapiLoopbackCapture(0))
             {
                 Capture.Initialize();
                 using (Source = new SoundInSource(Capture))
                 {
                     Source.DataAvailable += DataAvailable;
-
-                    Console.Write("IP? Empty for ADB: ");
-
-                    IPAddress IPAddr;
-                    var IP = Console.ReadLine();
-                    if (IP == string.Empty)
-                    {
-                        IPAddr = IPAddress.Loopback;
-                        Process.Start("adb", "forward tcp:1420 tcp:1420");
-                    }
-                    else
-                        IPAddr = IPAddress.Parse(IP);
-
-                    End = new IPEndPoint(IPAddr, 1420);
                     Capture.Start();
 
-                    Console.WriteLine("Booted");
+                    Console.WriteLine("Started recording audio");
                     while (true)
                     {
+                        var NoSpamDelay = Task.Delay(1000);
+                        if (UseAdb)
+                            Process.Start(new ProcessStartInfo()
+                            {
+                                FileName = "adb",
+                                Arguments = "forward tcp:1420 tcp:1420",
+                                UseShellExecute = false
+                            });
+
                         using (var Conn = new TcpClient()
                         {
                             NoDelay = true
@@ -52,17 +77,18 @@ namespace AudioStreamer
                         {
                             try
                             {
-                                Conn.Connect(End);
+                                Conn.Connect(Remote);
                                 Stream = Conn.GetStream();
                                 (DisconnectWaiter = new TaskCompletionSource<bool>()).Task.GetAwaiter().GetResult();
                             }
-                            catch (Exception Ex)
+                            catch
                             {
-                                Console.WriteLine(Ex);
                             }
 
                             Stream = null;
                         }
+
+                        NoSpamDelay.GetAwaiter().GetResult();
                     }
                 }
             }
